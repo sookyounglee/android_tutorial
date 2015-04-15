84422c306f2c33f15d39947dbc030f9711fa693c
jhjhpark <jhjh550@gmail.com>
2015-04-15 13:32:35 +0900

    tutorial 7 add

--- T07_thread/app/src/main/java/com/example/a/t07_thread/MainActivity.java
+++ T07_thread/app/src/main/java/com/example/a/t07_thread/MainActivity.java
@@ -26,6 +26,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyThread th = new MyThread();
+        th.start();
     }
 
 
