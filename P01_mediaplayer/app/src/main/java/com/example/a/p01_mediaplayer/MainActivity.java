f6124f82d823d889c8623d098f5de39fdf059337
jhjhpark <jhjh550@gmail.com>
2015-04-15 16:24:28 +0900

    p

---
+++ P01_mediaplayer/app/src/main/java/com/example/a/p01_mediaplayer/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.a.p01_mediaplayer;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String path = Environment.getExternalStorageDirectory().toString();
+        path += "/Music";
+
+        File f = new File(path);
+        File[] files = f.listFiles();
+
+        for(int i=0; i<files.length; i++){
+            files[i].getName();
+        }
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
