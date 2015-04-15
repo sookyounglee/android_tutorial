1dc3e66cb568ae2edab99962a7489a1f4a2906b6
jhjhpark <jhjh550@gmail.com>
2015-04-15 13:05:15 +0900

    init

---
+++ T02_Activity/app/src/main/java/com/example/a/t02_activity/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.a.t02_activity;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editID = (EditText)findViewById(R.id.editID);
+                EditText editPW = (EditText)findViewById(R.id.editPW);
+
+                String strID = editID.getText().toString();
+                String strPW = editPW.getText().toString();
+
+                Intent i = new Intent(MainActivity.this, MyActivity.class);
+                i.putExtra("myID", strID);
+                i.putExtra("myPW", strPW);
+                startActivity(i);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ T02_Activity/app/src/main/java/com/example/a/t02_activity/MyActivity.java
@@ -0,0 +1,28 @@
+package com.example.a.t02_activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+/**
+ * Created by a on 2015-04-14.
+ */
+public class MyActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_test);
+
+        Intent myIntent = getIntent();
+        String strID = myIntent.getExtras().get("myID").toString();
+        String strPW = myIntent.getExtras().get("myPW").toString();
+
+        TextView textID = (TextView)findViewById(R.id.text1);
+        TextView textPW = (TextView)findViewById(R.id.text2);
+
+        textID.setText("id: "+strID);
+        textPW.setText("pw: "+strPW);
+    }
+}
+
