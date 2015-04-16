1348efd362ca6fde48607604374185d1152855a4
jhjhpark <jhjh550@gmail.com>
2015-04-16 11:09:47 +0900

    11

--- T11_sqlite/app/src/main/java/com/example/a/t11_sqlite/T11SQLOpenHelper.java
+++ T11_sqlite/app/src/main/java/com/example/a/t11_sqlite/T11SQLOpenHelper.java
@@ -16,11 +16,16 @@ public class T11SQLOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String str = "create table student (id integer primary key autoincrement,"
+                + "name text, age integer, address text)";
+        db.execSQL(str);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String str = "drop table if exists student";
+        db.execSQL(str);
 
+        onCreate(db);
     }
 }
---
+++ T11_sqlite/app/src/main/java/com/example/a/t11_sqlite/T11SQLiteHandler.java
@@ -0,0 +1,16 @@
+package com.example.a.t11_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by a on 2015-04-16.
+ */
+public class T11SQLiteHandler {
+    T11SQLOpenHelper helper;
+    SQLiteDatabase db;
+
+    public T11SQLiteHandler(Context context){
+
+    }
+}
