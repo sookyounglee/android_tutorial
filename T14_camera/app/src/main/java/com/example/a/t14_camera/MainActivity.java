2a79fcb50a529b33394a512710bba7e246302e59
jhjhpark <jhjh550@gmail.com>
2015-04-16 15:49:54 +0900

    14

--- T14_camera/app/src/main/java/com/example/a/t14_camera/MainActivity.java
+++ T14_camera/app/src/main/java/com/example/a/t14_camera/MainActivity.java
@@ -1,19 +1,59 @@
 package com.example.a.t14_camera;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final int TAKE_PICTURE = 1;
+    String path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        path = Environment.getExternalStorageDirectory().toString()+"/imageTest.jpg";
+        Button btnTake = (Button)findViewById(R.id.btnTake);
+        btnTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(path)));
+                startActivityForResult(i, TAKE_PICTURE);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == TAKE_PICTURE){
+            if(resultCode == RESULT_OK){
+                File file = new File(path);
+                Bitmap bitmap = null;
+
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(
+                        getContentResolver(), Uri.fromFile(file));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
