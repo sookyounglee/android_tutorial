588c6ee1406a3cccfdc0e233e8e806119e99d876
jhjhpark <jhjh550@gmail.com>
2015-04-17 11:15:59 +0900

    15

--- T15_alertDialog/app/src/main/java/com/example/a/t15_alertdialog/MainActivity.java
+++ T15_alertDialog/app/src/main/java/com/example/a/t15_alertdialog/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.a.t15_alertdialog;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int DIALOG_TEST = 1;
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id){
+            case DIALOG_TEST:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("종료 확인");
+                builder.setMessage("어플리케이션을 종료하시겠습니까?");
+                builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Destroy canceled", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                return dialog;
+        }
+
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btnAlertDialog);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_TEST);
+            }
+        });
     }
 
 
