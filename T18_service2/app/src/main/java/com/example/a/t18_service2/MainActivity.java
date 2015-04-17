8bcd1f008b15ec02ffc15910896e8d5f4319248f
jhjhpark <jhjh550@gmail.com>
2015-04-17 17:17:20 +0900

    18

--- T18_service2/app/src/main/java/com/example/a/t18_service2/MainActivity.java
+++ T18_service2/app/src/main/java/com/example/a/t18_service2/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.a.t18_service2;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,6 +37,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnGetRandom = (Button)findViewById(R.id.btnStart);
+        btnGetRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isBound){
+                    int num = myService.getRandomNubmer();
+                    Toast.makeText(MainActivity.this, "number : "+num, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
