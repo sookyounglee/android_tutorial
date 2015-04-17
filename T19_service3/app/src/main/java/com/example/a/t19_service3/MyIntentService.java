e9f0f1a15a1d79d96002ceba4e1e002b737badfb
jhjhpark <jhjh550@gmail.com>
2015-04-17 17:39:18 +0900

    19

--- T19_service3/app/src/main/java/com/example/a/t19_service3/MyIntentService.java
+++ T19_service3/app/src/main/java/com/example/a/t19_service3/MyIntentService.java
@@ -8,13 +8,9 @@ import android.util.Log;
  * Created by a on 2015-04-17.
  */
 public class MyIntentService extends IntentService{
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public MyIntentService(String name) {
-        super(name);
+
+    public MyIntentService() {
+        super("MyIntentService");
     }
 
     @Override
