ede2109d1bca9e12d57c65c53857000264c294da
jhjhpark <jhjh550@gmail.com>
2015-04-17 17:42:16 +0900

    19

--- T19_service3/app/src/main/java/com/example/a/t19_service3/MainActivity.java
+++ T19_service3/app/src/main/java/com/example/a/t19_service3/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.a.t19_service3;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button)findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyIntentService.class);
+                startService(i);
+            }
+        });
+        Button btnStop = (Button)findViewById(R.id.btnStop);
+        btnStop.
     }
 
 
