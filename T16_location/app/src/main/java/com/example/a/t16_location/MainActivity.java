11cd5f97be366e3e0b85bca64bae592e683dad33
jhjhpark <jhjh550@gmail.com>
2015-04-17 14:00:28 +0900

    16

--- T16_location/app/src/main/java/com/example/a/t16_location/MainActivity.java
+++ T16_location/app/src/main/java/com/example/a/t16_location/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a.t16_location;
 
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
     }
 
 
