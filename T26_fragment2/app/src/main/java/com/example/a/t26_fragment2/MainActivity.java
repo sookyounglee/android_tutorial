86a7c4e38ddcb876ea8dddc728d299fb68301ba4
jhjhpark <jhjh550@gmail.com>
2015-04-20 15:56:51 +0900

    26

--- T26_fragment2/app/src/main/java/com/example/a/t26_fragment2/MainActivity.java
+++ T26_fragment2/app/src/main/java/com/example/a/t26_fragment2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a.t26_fragment2;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
@@ -22,6 +23,7 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    FragmentManager fm = getFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
