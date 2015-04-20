8ef06f73e0880190b13844020933cb4feaf9d6e2
jhjhpark <jhjh550@gmail.com>
2015-04-20 15:05:40 +0900

    25

---
+++ T25_fragment/app/src/main/java/com/example/a/t25_fragment/MyFragment.java
@@ -0,0 +1,20 @@
+package com.example.a.t25_fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by a on 2015-04-20.
+ */
+public class MyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    }
+}
