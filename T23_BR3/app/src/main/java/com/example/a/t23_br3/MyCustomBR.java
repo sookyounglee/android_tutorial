868aef009b445aa107eb95c7c6da3d594032177c
jhjhpark <jhjh550@gmail.com>
2015-04-20 13:38:56 +0900

    23

--- T23_BR3/app/src/main/java/com/example/a/t23_br3/MyCustomBR.java
+++ T23_BR3/app/src/main/java/com/example/a/t23_br3/MyCustomBR.java
@@ -1,16 +1,19 @@
 package com.example.a.t23_br3;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 /**
  * Created by a on 2015-04-20.
  */
 public class MyCustomBR extends BroadcastReceiver {
+    NotificationManager manager;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "BroadCast Receiver!!", Toast.LENGTH_LONG).show();
+        //Toast.makeText(context, "BroadCast Receiver!!", Toast.LENGTH_LONG).show();
+        manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 }
