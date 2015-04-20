6ef7843c01f9feb5f31f65c68ab1088ca6a62f32
jhjhpark <jhjh550@gmail.com>
2015-04-20 10:31:44 +0900

    21

---
+++ T21_BR/app/src/main/java/com/example/a/t21_br/MyBR.java
@@ -0,0 +1,15 @@
+package com.example.a.t21_br;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by a on 2015-04-20.
+ */
+public class MyBR extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
