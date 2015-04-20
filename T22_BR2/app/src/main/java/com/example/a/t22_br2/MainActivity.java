cea65170966744836d45495b30b9ace25c36ddc2
jhjhpark <jhjh550@gmail.com>
2015-04-20 11:31:30 +0900

    22

--- T22_BR2/app/src/main/java/com/example/a/t22_br2/MainActivity.java
+++ T22_BR2/app/src/main/java/com/example/a/t22_br2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a.t22_br2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -27,6 +28,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(receiver,filter);
     }
 
     @Override
