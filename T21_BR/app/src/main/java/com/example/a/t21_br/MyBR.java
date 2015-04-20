2e4593d7bc4fd15e25638a5177eeacd3b2c420a7
jhjhpark <jhjh550@gmail.com>
2015-04-20 10:43:45 +0900

    21

--- T21_BR/app/src/main/java/com/example/a/t21_br/MyBR.java
+++ T21_BR/app/src/main/java/com/example/a/t21_br/MyBR.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 /**
  * Created by a on 2015-04-20.
@@ -19,8 +20,11 @@ public class MyBR extends BroadcastReceiver {
             Object[] pdus = (Object[]) bundle.get("pdus");
             msgs = new SmsMessage[pdus.length];
             for(int i=0; i<msgs.length; i++){
-
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                str += "SMS from : "+msgs[i].getOriginatingAddress() + "\n";
+                str += msgs[i].getMessageBody()+"\n";
             }
+            Toast.makeText(context, str, Toast.LENGTH_LONG).show();
         }
     }
 }
