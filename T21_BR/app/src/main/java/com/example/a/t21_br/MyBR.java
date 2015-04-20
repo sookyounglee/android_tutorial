62582d006efebebe6d8287b1200b5b59d6165df5
jhjhpark <jhjh550@gmail.com>
2015-04-20 10:39:52 +0900

    21

--- T21_BR/app/src/main/java/com/example/a/t21_br/MyBR.java
+++ T21_BR/app/src/main/java/com/example/a/t21_br/MyBR.java
@@ -3,6 +3,8 @@ package com.example.a.t21_br;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
 
 /**
  * Created by a on 2015-04-20.
@@ -10,6 +12,15 @@ import android.content.Intent;
 public class MyBR extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "";
+        if(bundle != null){
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for(int i=0; i<msgs.length; i++){
 
+            }
+        }
     }
 }
